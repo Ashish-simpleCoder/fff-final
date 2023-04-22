fa2a1d849af61e06d3118803d2dd9c0548cf4dd6
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2023-04-22 20:45:25 +0530

    Add comb

---
+++ .changeset/fifty-singers-allow.md
@@ -0,0 +1,5 @@
+---
+"fff-final": patch
+---
+
+Add comb
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-   entry: ['src/index.tsx', "src/hooks/index.tsx"],
+   entry: ['src/index.tsx'],
    splitting: true,
    sourcemap: true,
    clean: true,
@@ -13,7 +13,6 @@ export default defineConfig({
    shims: true,
    treeshake: true,
    jsxFragment: "React.Fragment",
-   legacyOutput: true,
    metafile: true,
    tsconfig: "./tsconfig.json"
 })
